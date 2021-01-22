dab52c6f62fef706df697f1d29469b1d36b69eb0
John Cupitt <jcupitt@gmail.com>
2021-01-22 12:31:06 +0000

    add redirect from old home

---
+++ info.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html lang="en-us">
+  <head>
+    <meta charset="UTF-8">
+    <!-- redirect from the old website homepage -->
+    <meta http-equiv="refresh" content="0; URL='http://susancupitt.co.uk'"/>
+  </head>
+</html>
