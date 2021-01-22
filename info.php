672a236706bf00a913da1a5a31f8f7576c7a955b
John Cupitt <jcupitt@gmail.com>
2021-01-22 12:37:35 +0000

    revert, not a good idea heh

--- info.php
+++
@@ -1,8 +0,0 @@
-<!DOCTYPE html>
-<html lang="en-us">
-  <head>
-    <meta charset="UTF-8">
-    <!-- redirect from the old website homepage -->
-    <meta http-equiv="refresh" content="0; URL='http://susancupitt.co.uk'"/>
-  </head>
-</html>
